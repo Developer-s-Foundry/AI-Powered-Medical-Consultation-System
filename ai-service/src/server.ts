c40892fa2ce6c44502c5ee7bac372d1ab01263ae
bobprince <bobprince87@gmail.com>
2026-03-22 02:22:12 +0100

    modified:   ai-service/src/server.ts

--- ai-service/src/server.ts
+++ ai-service/src/server.ts
@@ -9,7 +9,7 @@ import { initializeSocket } from "./config/socket";
 
 (async () => {
   const logger = Logger.getInstance();
-  const port = process.env.PORT || process.env.PORT || 3001;
+  const port = process.env.PORT || process.env.SERVER_PORT || 3001;
 
   const app = express();
   const server = http.createServer(app);
