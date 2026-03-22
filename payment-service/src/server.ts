50555cac73d4a27690a4a7dd1951b6bd8e054b2f
bobprince <bobprince87@gmail.com>
2026-03-22 03:19:44 +0100

    modified:   payment-service/src/server.ts

--- payment-service/src/server.ts
+++ payment-service/src/server.ts
@@ -1,37 +1,34 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import AppDataSource from './config/database';
-import { Logger } from './config/logger';
+import dotenv from "dotenv";
+import express from "express";
+import AppDataSource from "./config/database";
+import { Logger } from "./config/logger";
 import { Response as ExResponse, Request as ExRequest } from "express";
 import swaggerUi from "swagger-ui-express";
-import { RegisterRoutes } from './swagger/routes'
-
-
+import { RegisterRoutes } from "./swagger/routes";
 
 (async () => {
+  dotenv.config();
+  const logger = Logger.getInstance();
 
-    dotenv.config();
-    const logger = Logger.getInstance();
+  const port = process.env.PORT;
+  const app: express.Application = express();
+  app.use(express.json());
+  RegisterRoutes(app);
+  // Swagger ui setup
+  app.use("/api", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+    return res.send(
+      swaggerUi.generateHTML(await import("./swagger/swagger.json")),
+    );
+  });
 
-    const port = process.env.PORT;
-    const app: express.Application = express();
-    app.use(express.json());
-    RegisterRoutes(app)
-        // Swagger ui setup
-    app.use("/api", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
-        return res.send(
-            swaggerUi.generateHTML(require("./swagger/swagger.json"))
-        );
+  try {
+    await AppDataSource.initialize();
+    console.log("Database connection established successfully.");
+    app.listen(port, () => {
+      logger.logToConsole();
+      logger.info(`Server is running on port ${port}`);
     });
-    
-    try {
-       await AppDataSource.initialize();
-       console.log('Database connection established successfully.');
-       app.listen(port, () => {
-            logger.logToConsole();
-            logger.info(`Server is running on port ${port}`);
-       });  
-    } catch (error) {
-        logger.error('Error starting server:', error);
-    }
+  } catch (error) {
+    logger.error("Error starting server:", error);
+  }
 })();
